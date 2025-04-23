2af24dfc11e6b1c989d7abda9a8d2fc91df78c60
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-04-22 19:26:15 -0700

    Revert "Fixes for MacOS 15.4"
    Turns out these changes resulted in a broken build. For now, we have to just build with SIP disabled (NOT required for installation of the resulting package) until we figure out how to keep SIP from breaking things during the build.

--- configs/components/puppet-runtime.rb
+++ configs/components/puppet-runtime.rb
@@ -48,17 +48,10 @@ component 'puppet-runtime' do |pkg, settings, platform|
     ]
   elsif platform.is_macos?
     # We can't untar into '/' because of SIP on macOS; Just copy the contents
-    # of these directories instead. Also, /private got a lot more restrictive
-    # in 15.4, and most of the pieces of the -a flag end up being not permitted.
-    # Because basically nothing ends up there (it's an empty /private/etc/puppetlabs),
-    # just do -k. The var dir is also usually not present, so just skip it if it
-    # doesn't exist. Still fail if opt doesn't, because if not, something is
-    # very wrong.
+    # of these directories instead:
     install_command = [
       "tar -xzf #{tarball_name}",
-      'd="opt";rsync -ka "$${d}/" "/$${d}/"',
-      'd="var";if [ -d $${d} ];then rsync -ka "$${d}/" "/$${d}/";fi',
-      'd="private";if [ -d $${d} ];then rsync -k "$${d}/" "/$${d}/";fi',
+      "for d in opt var private; do rsync -ka \"$${d}/\" \"/$${d}/\"; done"
     ]
   else
     install_command = ["gunzip -c #{tarball_name} | #{platform.tar} -k -C / -xf -"]
