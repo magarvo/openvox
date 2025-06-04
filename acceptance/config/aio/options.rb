261c0927a48453761a848af2d9185b790163eafd
Josh Partlow <jpartlow@glatisant.org>
2025-06-04 15:57:46 -0700

    (gh-101) Remove archive logs post_suite step from Beaker acceptance
    The step bundled up log tarballs which Jira was configured to extract
    and preserver for inspection. We might want to do something else here
    now, but for the time being I'm just unhooking it as we don't have
    anywhere to store them.

--- acceptance/config/aio/options.rb
+++ acceptance/config/aio/options.rb
@@ -5,7 +5,4 @@
   'puppetservice'              => 'puppetserver',
   'puppetserver-confdir'       => '/etc/puppetlabs/puppetserver/conf.d',
   'puppetserver-config'        => '/etc/puppetlabs/puppetserver/conf.d/puppetserver.conf',
-  :post_suite => [
-    'teardown/common/099_Archive_Logs.rb',
-  ],
 }
