magarvo/openvox

commit 83d06f38a68eb128e45ad1cf5fd2db35d56aec2f

Provide puppet-agent on RPM and Debian
This allows users to perform `dnf install puppet-agent` or
`apt install puppet-agent` and get openvox-agent instead. It also means
packages don't need to modify their dependencies. They probably still
should, but this helps users migrate.

It also tightens the Obsoletes statement to include the major version.
rpmlint raises the unversioned-explicit-obsoletes warning on the old
case. The Conflicts is kept unversioned, which is correct.

configs/projects/openvox-agent.rb
@@ -176,8 +176,13 @@ project 'openvox-agent' do |proj|
   end
 
   # make sure we can replace puppet-agent in place for the rename
-  proj.replaces 'puppet-agent'
+  proj.replaces 'puppet-agent', "< #{major.to_i + 1}"
   proj.conflicts 'puppet-agent'
+  if platform.is_rpm?
+    proj.provides 'puppet-agent', "= #{major}"
+  elsif platform.is_deb?
+    proj.provides 'puppet-agent', "(= #{major})"
+  end
 
   proj.timeout 7200 if platform.is_windows?
 end

commit a54b11190d9041c1fd53a35f1d919e157e6ccdc9

Correct provides statement for Debian packaging
In 83d06f38a68eb128e45ad1cf5fd2db35d56aec2f I tried to add a proper
provides, but it turns out Vanagon completely ignores any version
constraint. Once it does, it shouldn't need the braces around it. Then
it's the same as RPM and the code can be simplified.

Fixes: 83d06f38a68e ("Provide puppet-agent on RPM and Debian")

configs/projects/openvox-agent.rb
@@ -160,11 +160,7 @@ project 'openvox-agent' do |proj|
   # make sure we can replace puppet-agent in place for the rename
   proj.replaces 'puppet-agent', "< #{major.to_i + 1}"
   proj.conflicts 'puppet-agent'
-  if platform.is_rpm?
-    proj.provides 'puppet-agent', "= #{major}"
-  elsif platform.is_deb?
-    proj.provides 'puppet-agent', "(= #{major})"
-  end
+  proj.provides 'puppet-agent', "= #{major}"
 
   proj.timeout 7200 if platform.is_windows?
 end