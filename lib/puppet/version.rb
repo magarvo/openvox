24a6fdd129a99cfb9526b8ff7330e5dd7c6558a0
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2025-09-08 22:50:55 +0000

    Release 8.23.1

--- lib/puppet/version.rb
+++ lib/puppet/version.rb
@@ -8,7 +8,7 @@
 # Raketasks and such to set the version based on the output of `git describe`
 
 module Puppet
-  PUPPETVERSION = '8.23.0'
+  PUPPETVERSION = '8.23.1'
   IMPLEMENTATION = 'openvox'
 
   ##
