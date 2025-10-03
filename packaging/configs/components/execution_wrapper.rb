276f767a10e2c978f91c411f992e95420b46b3d9
magarvo <ma_garvo@hotmail.com>
2025-10-03 12:34:09 +0100

    adding execution_wrapper.bat for windows

--- packaging/configs/components/execution_wrapper.rb
+++ packaging/configs/components/execution_wrapper.rb
@@ -3,4 +3,9 @@
 component "execution_wrapper" do |pkg, settings, platform|
   pkg.add_source "file://resources/files/execution_wrapper"
   pkg.install_file "execution_wrapper", "#{settings[:bindir]}/execution_wrapper", mode: '0755'
+
+  if platform.is_windows?
+    pkg.add_source "file://resources/files/windows/execution_wrapper.bat"
+    pkg.install_file "execution_wrapper.bat", "#{settings[:bindir]}/execution_wrapper.bat"
+  end
 end
